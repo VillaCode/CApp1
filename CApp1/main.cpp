ec602ebe0b9861fd972a21e8516226bce9939bb1
kevin <kevinvillab89@gmail.com>
2025-01-26 21:58:39 -0600

    funciona

--- CApp1/main.cpp
+++ CApp1/main.cpp
@@ -10,7 +10,6 @@ void imprimeArreglo(float* d);
 
 int main() {
     std::cout << "Sumando Arreglos en Paralelo!\n";
-
     float a[N], b[N], c[N];
     int i;
 
@@ -21,5 +20,28 @@ int main() {
 
     int pedazos = chunk;
 
+#pragma omp parallel for \
+    shared(a, b, c, pedazos) private(i) \
+    schedule(static, pedazos)
+    for (i = 0; i < N; i++) {
+        c[i] = a[i] + b[i];
+    }
+
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
+    imprimeArreglo(a);
+
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
+    imprimeArreglo(b);
+
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo c: " << std::endl;
+    imprimeArreglo(c);
+
     return 0;
+}
+
+void imprimeArreglo(float* d) {
+    for (int x = 0; x < mostrar; x++) {
+        std::cout << d[x] << " - ";
+    }
+    std::cout << std::endl;
 }
